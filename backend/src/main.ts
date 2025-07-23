19244deaa0d01de56eb959d06345045b67cff4cc
Parthasrikar <114384370+Parthasrikar@users.noreply.github.com>
2025-07-23 17:55:14 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Your Angular app URL
+    origin: 'todo-angular-ad46.onrender.com', // Your Angular app URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
